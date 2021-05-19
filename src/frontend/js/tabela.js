09c5061121b990b4324ddc4f7dda01bbc8c208b0
lolphbgg@gmail.com <lolphbgg@gmail.com>
2021-05-18 21:56:22 -0300

    tabela gerada pelo js

--- src/frontend/js/tabela.js
+++ src/frontend/js/tabela.js
@@ -1,48 +1,123 @@
+// $(document).ready(function () {
+// 	console.log('Ready disparado');
+
+// 	$(".btnInserir").on("click", function () {
+// 		var $botaoInserir = $(this);
+
+// 		console.log("O botão clicado foi: ", $botaoInserir.text(), " valor: ", $botaoInserir.val());
+// 		validarForm();
+// 	});
+
+// 	$(".btnEditar").on("click", function () {
+// 		var $botaoEditar = $(this);
+
+// 		console.log("O botão clicado foi: ", $botaoEditar.text(), " valor: ", $botaoEditar.val());
+// 	});
+
+
+// 	// Excluir Registro do Banco e da tela (Tabela)
+// 	$('#tabelaTarefas').on('click', '.btnExcluir', function (e) {
+// 		e.preventDefault();
+// 		var $botaoExcluir = $(this);
+
+// 		console.log("O botão clicado foi: ", $botaoExcluir.text(), " valor: ", $botaoExcluir.val());
+
+// 		$botaoExcluir.closest('tr').remove();
+// 	});
+
+// 	// Example starter JavaScript for disabling form submissions if there are invalid fields
+// 	function validarForm() {
+// 		'use strict'
+
+// 		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+// 		var forms = document.querySelectorAll('.needs-validation')
+
+// 		// Loop over them and prevent submission
+// 		Array.prototype.slice.call(forms)
+// 			.forEach(function (form) {
+// 				form.addEventListener('submit', function (event) {
+// 					if (!form.checkValidity()) {
+// 						event.preventDefault()
+// 						event.stopPropagation()
+// 					}
+
+// 					form.classList.add('was-validated')
+// 				}, false)
+// 			})
+// 	}
+// });
+
+
+
 $(document).ready(function () {
-	console.log('Ready disparado');
 
-	$(".btnInserir").on("click", function () {
-		var $botaoInserir = $(this);
+var tabelatarefas = $("#tabelaTarefas")
+montartabela();
 
-		console.log("O botão clicado foi: ", $botaoInserir.text(), " valor: ", $botaoInserir.val());
-		validarForm();
-	});
+function montartabela() {
+	var cabecalho = montarcabecalhotabela();
+    var corpo = montarCorpoTabela();
+    var htmldatabela= (cabecalho + corpo);
 
-	$(".btnEditar").on("click", function () {
-		var $botaoEditar = $(this);
+	tabelatarefas.html(htmldatabela)
 
-		console.log("O botão clicado foi: ", $botaoEditar.text(), " valor: ", $botaoEditar.val());
-	});
+}
 
+function montarCabecalhoTabela() {
+    var cabecalho = (
+		'<thead class="table-dark">' +
+			'<tr>' +
+				'<th scope="col">Id</th>' +
+				'<th scope="col">Tarefa</th>' +
+				'<th scope="col">Usuário</th>' +
+				'<th scope="col">Data</th>' +
+				'<th scope="col">Ações</th>' +
+			'</tr>' +
+		'</thead>'
+	 );
 
-	// Excluir Registro do Banco e da tela (Tabela)
-	$('#tabelaTarefas').on('click', '.btnExcluir', function (e) {
-		e.preventDefault();
-		var $botaoExcluir = $(this);
+    return cabecalho;
+}
 
-		console.log("O botão clicado foi: ", $botaoExcluir.text(), " valor: ", $botaoExcluir.val());
+function montarCorpoTabela() {
+      var corpo = (
 
-		$botaoExcluir.closest('tr').remove();
-	});
+		'<tbody>'+
+			'<tr>'+
+				'<td>1</td>'+
+				'<td>Mark</td>'+
+				'<td>Otto</td>'+
+				'<td>@mdo</td>'+
+				'<td>'+
+					'<button type="button" class="btn btn-warning btnEditar" value=1>Editar</button>'+
+					'&nbsp;'+
+					'<button type="button" class="btn btn-danger btnExcluir" value=1>Excluir</button>'+
+				'</td>'+
+			'</tr>'+
+			'<tr>'+
+				'<td>2</td>'+
+				'<td>Jacob</td>'+
+				'<td>Thornton</td>'+
+				'<td>@fat</td>'+
+				'<td>'+
+					'<button type="button" class="btn btn-warning btnEditar" value=2>Editar</button>'+
+					'&nbsp;'+
+					'<button type="button" class="btn btn-danger btnExcluir" value=2>Excluir</button>'+
+				'</td>'+
+			'</tr>'+
+			'<tr>'+
+				'<td>3</td>'+
+				'<td colspan="2">Larry the Bird</td>'+
+				'<td>@twitter</td>'+
+				'<td>'+
+					'<button type="button" class="btn btn-warning btnEditar" value=3>Editar</button>'+
+					'&nbsp;'+
+					'<button type="button" class="btn btn-danger btnExcluir" value=3>Excluir</button>'+
+				'</td>'+
+			'</tr>'+
+		'</tbody>'
+	);
+	return corpo;
 
-	// Example starter JavaScript for disabling form submissions if there are invalid fields
-	function validarForm() {
-		'use strict'
-
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = document.querySelectorAll('.needs-validation')
-
-		// Loop over them and prevent submission
-		Array.prototype.slice.call(forms)
-			.forEach(function (form) {
-				form.addEventListener('submit', function (event) {
-					if (!form.checkValidity()) {
-						event.preventDefault()
-						event.stopPropagation()
-					}
-
-					form.classList.add('was-validated')
-				}, false)
-			})
-	}
+}
 });
